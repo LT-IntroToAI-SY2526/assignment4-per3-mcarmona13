02749c75c34d043926d06e4315154ac622802060
Maximiliano Carmona <mcarmona17@cps.edu>
2025-10-24 10:36:18 -0500

    finished dog class

--- notes.py
+++ notes.py
@@ -12,7 +12,7 @@ class Dog:
         age - the age of the dog
         favorite_food - the dog's favorite food
     """
-    def __init__(self, breed, name, age, favorite_food):
+    def __init__(self, breed = "rotweiler", name = "cupcake", age = 5, favorite_food = "babies"):
         """Initialize a new Dog with breed, name, age, and favorite food"""
         self.breed = breed
         self.name = name
@@ -21,7 +21,7 @@ class Dog:
 
     def __str__(self):
         """return a string representation of a dog"""
-        return f"{self.name} is a {self.age} year old {self.breed} dog who loves {self.favorite_food}"
+        return f"{self.name} is a {self.age} year old {self.breed} dog who loves to eat {self.favorite_food}"
 
     def bark(self):
         """Make the dog bark"""
@@ -32,15 +32,48 @@ class Dog:
         dog asks for food politly
         """
         return f"yo, give me FOOD now son"
+    
+    def birthday(self):
+        """
+        Celebrates the dog's birthday
+        """
+        self.age += 1
+        print(f"Celebrating {self.name}'s birhtday, they are now {self.age} years old.")
+
+    def change_favorite_food(self, new_food):
+        """
+        Change the favorite food of the dog
+        """
+        old_food = self.favorite_food
+        self.favorite_food = new_food
+        print(f"{self.name} use to love {old_food} and now loves {self.favorite_food}")
+
 
 
 
 
 
 my_dog = Dog("golden retriever", "mack", 4, "cheez its")
+default_dog = Dog()
+print() 
+print() 
 print(my_dog)
+print(my_dog.bark())
+print(my_dog.hunger())
+print() 
+print() 
+print(default_dog)
+print() 
+print() 
+enggy_dog = Dog("black and white", "peluchin", 13, "rice")
+aaron_dog = Dog("peach and white", "dumbo", favorite_food="anything edible")
+
+print(enggy_dog)
+print(aaron_dog)
 
 print() 
 
-print(my_dog.bark())
-print(my_dog.hunger())
+print(enggy_dog.bark())
+aaron_dog.birthday()
+
+print(aaron_dog)
